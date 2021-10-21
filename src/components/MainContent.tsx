cf65ded64eb8972132acab934aee242fd874e07e
Martha Bowler <mbowler133@gmail.com>
2021-10-21 13:47:30 +0100

    Add Main Content

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,13 @@
+import AppHeader from "./AppHeader";
+import Places from "./Places";
+
+function MainContent(): JSX.Element {
+  return (
+    <main>
+      <AppHeader />
+      <Places />
+    </main>
+  );
+}
+
+export default MainContent;
