6d1bee788ea7b537de15ca6f1f0bc1e8b708c754
Martha Bowler <mbowler133@gmail.com>
2021-10-21 16:48:17 +0100

    Add table of contents

---
+++ src/components/Table.tsx
@@ -0,0 +1,22 @@
+function TableContents(): JSX.Element {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>Navigate to...</th>
+        </tr>
+        <tr>
+          <td>
+            <a href="#Ravello">Ravello</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="#Borough Market">Borough Market</a>
+          </td>
+        </tr>
+      </table>
+    </>
+  );
+}
+export default TableContents;
