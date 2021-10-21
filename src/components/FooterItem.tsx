265489707f7f370d5f88ca849582df677dbd14ba
Martha Bowler <mbowler133@gmail.com>
2021-10-21 15:05:14 +0100

    Check app builds

--- src/components/FooterItem.tsx
+++ src/components/FooterItem.tsx
@@ -1,18 +1,14 @@
-import AppHeader from "./AppHeader";
-
 interface FooterProps {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 }
-
-function FooterItem(props: FooterProps): JSX.Element {
-  return (
-    <>
-      <p>
-        Photo credits to :<a href={props.url}>{props.name}</a>
-      </p>
-    </>
-  );
-}
-
-export default FooterItem;
+  
+  function FooterItem(props: FooterProps): JSX.Element {
+    return (
+      <>
+          <p>Photo credits to w:<a href= {props.url}>{props.name}</a></p>
+      </>
+    );
+  }
+  
+  export default FooterItem;
