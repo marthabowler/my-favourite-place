879f6dd85e8ad5e05ff1ba151db86f0b23dcbb95
Martha Bowler <mbowler133@gmail.com>
2021-10-21 13:46:30 +0100

    Add Place Item template

---
+++ src/components/PlaceItem.tsx
@@ -0,0 +1,23 @@
+interface PlacesProps {
+  title: string;
+  PlaceName: string;
+  CountryName: string;
+  imageUrl: string;
+  imageAlt: string;
+  intro: string;
+}
+
+function PlaceItem(props: PlacesProps): JSX.Element {
+  return (
+    <section>
+      <img src={props.imageUrl} alt={props.imageAlt} />
+      <h2>{props.title}</h2>
+      <h3>
+        {props.PlaceName} {","} {props.CountryName}
+      </h3>
+      <p>{props.intro}</p>
+    </section>
+  );
+}
+
+export default PlaceItem;
