8938d7242806f5aaaffece0d53b11d9379dd534c
Martha Bowler <mbowler133@gmail.com>
2021-10-22 11:36:23 +0100

    Add map method

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,17 +1,40 @@
 import FooterItem from "./FooterItem";
 
 function AppFooter(): JSX.Element {
+  // return with map
+  const footerData = [
+    {
+      url: "https://www.istockphoto.com/portfolio/bluejayphoto?mediatype=photography",
+      name: "bluejayphoto",
+    },
+    {
+      url: "https://www.istockphoto.com/portfolio/visualspace?mediatype=photography",
+      name: "visualspace",
+    },
+  ];
+
   return (
     <footer>
-      <FooterItem
-        url="https://www.istockphoto.com/portfolio/bluejayphoto?mediatype=photography"
-        name="bluejayphoto"
-      />
-      <FooterItem
-        url="https://www.istockphoto.com/portfolio/visualspace?mediatype=photography"
-        name="visualspace"
-      />
+      {footerData.map((loc, index: number) => (
+        <FooterItem url={loc.url} name={loc.name} key={index} />
+      ))}
     </footer>
   );
 }
+
+//
 export default AppFooter;
+
+// <footer>
+//   //return without map
+//       <FooterItem
+//         url="https://www.istockphoto.com/portfolio/bluejayphoto?mediatype=photography"
+//         name="bluejayphoto"
+//       />
+//       <FooterItem
+//         url="https://www.istockphoto.com/portfolio/visualspace?mediatype=photography"
+//         name="visualspace"
+//       />
+//     </footer>
+//   );
+// }
